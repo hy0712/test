cfb687ad0f6dedb1ec3acdcd652ed2c57b8af6a3
905524534@qq.com <905524534@qq.com>
2020-01-29 18:06:11 +0800

    add

--- read_json.py
+++
@@ -1,7 +0,0 @@
-import json
-
-# json.load()函数的使用，将读取json信息
-
-file = open('1.json', 'r', encoding='utf-8')
-info = json.load(file)
-print(info)
--- wirte_to_json.py
+++
@@ -1,6 +0,0 @@
-import json
-
-# json.dump()函数的使用，将json信息写进文件
-json_info = "{'age':'12'}"
-file = open('1.json', 'w', encoding='utf-8')
-json.dump(json_info, file)
